0d7613710bff7182b6f246d4bfc83555963c58d0
amartelr <alfredo.martel@gmail.com>
2018-02-10 22:34:09 +0000

    testing atom

--- holago.go
+++ holago.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
+// Servidor http
 func Servidor(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hola Mundo!\n")
 }
 
 func main() {
+	fmt.Println("http://localhost/holago:8888")
 	http.HandleFunc("/holago", Servidor)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
