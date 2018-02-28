ec6a218a7a483eb5573f45f26a5f5d575d569b6d
amartelr <alfredo.martel@gmail.com>
2018-02-28 21:39:19 +0000

    Usando ServerMux

--- holago.go
+++ holago.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
-// Servidor http
-func Servidor(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "<b>Hola Mundo!</b>\n")
+// HolaMundo http
+func HolaMundo(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<h1>Hola Mundo!</h1>")
+}
+
+// Usuario http
+func Usuario(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<h1>Hola Usuario</h1>")
+}
+
+type mensaje struct {
+	msg string
+}
+
+func (m mensaje) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, m.msg)
 }
 
 func main() {
-	fmt.Println("http://localhost/holago:8888")
-	http.HandleFunc("/holago", Servidor)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+
+	msg := mensaje{
+		msg: "Hola Nuevo Mundo",
+	}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HolaMundo)
+	mux.HandleFunc("/user", Usuario)
+	mux.Handle("/hola", msg)
+
+	server := &http.Server{
+		Addr:           ":8888",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Println("Escuchando ...")
+	log.Fatal(server.ListenAndServe())
 }
